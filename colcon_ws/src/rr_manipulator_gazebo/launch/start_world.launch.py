a8199fbaeace6a3da2d5419ee7adc06472f42d92
kumar-sanjeeev <kumar.san96@gmail.com>
2023-10-21 11:50:49 +0200

    add: ros2 control and gazebo related pkgs of rr manipulator robot

---
+++ colcon_ws/src/rr_manipulator_gazebo/launch/start_world.launch.py
@@ -0,0 +1,23 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+
+    # Gazebo Launch
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py')
+        )
+    )
+
+    return LaunchDescription(
+        [
+            gazebo
+        ]
+    )
